a8aff9ac0a0288d5d9b499c66b2f89ebabbcdcb5
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2019-07-26 19:37:39 +0900

    update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 #
 # (c) 2019 Yoichi Tanibayashi
 #
+BINDIR=${HOME}/bin
 TOPDIR=${HOME}/OttoPi
 
 cd ${TOPDIR}
 sudo pip3 install -r requirements.txt
+
+### OttoPi.conf
+if [ ! -f ${BINDIR}/OttoPi.conf ]; then
+	cp ${TOPDIR}/OttoPi.conf-sample ${BINDIR}/OttoPi.conf
+fi
