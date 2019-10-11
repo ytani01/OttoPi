5d2540ff86129c1e57bdbe7bb04ba52ae04ab33c
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2019-10-11 14:07:50 +0900

    update

--- loop.sh
+++ loop.sh
@@ -1,5 +1,16 @@
 #!/bin/sh -x
 #
+# (c) 2019 Yoichi Tanibayashi
+#
+
+SPEECH_TXT=${HOME}/OttoPi/speech.txt
+STOP_FILE=${HOME}/speech_stop
+
 while true; do
-    speech.sh
+    if [ -f ${STOP_FILE} ]; then
+	echo "stop file: ${STOP_FILE}"
+    else
+	cat $SPEECH_TXT | speech.sh ${STOP_FILE}
+    fi
+    sleep 1
 done
--- speech.sh
+++ speech.sh
@@ -1,24 +1,15 @@
 #!/bin/sh -x
 #
-T1=9
-T2=20
+# (c) Yoichi Tanibayashi
+#
 
-SpeakClient.py 'こんにちは'
-sleep $T1
-SpeakClient.py '私は二そくほこうロボット'
-SpeakClient.py 'オットーパイ と申します'
-sleep $T1
-SpeakClient.py '障害物を避けながら'
-SpeakClient.py '自動で 歩き回ります'
-sleep $T1
-SpeakClient.py 'ときどき踊ります'
-sleep $T1
-SpeakClient.py '私はラズベリーパイ ゼロ をベースに'
-SpeakClient.py 'サーボモーターと'
-SpeakClient.py 'ごく簡単な追加回路だけで作られています'
-sleep $T1
-SpeakClient.py 'ソフトウエアは'
-SpeakClient.py 'パイソン言語で書かれています'
-sleep $T1
-SpeakClient.py 'よろしくお願いいたします'
-sleep $T2
+STOP_FILE=$1
+
+while read s; do
+    if [ -f ${STOP_FILE} ]; then
+	echo "stop file: ${STOP_FILE}"
+	exit 0
+    fi
+    SpeakClient.py $s
+done
+sleep 30
